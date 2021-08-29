e82897ce865c88b90aa6d34a83f658c617b7b754
Mohamed Aberkani <aberkanimed@gmail.com>
2021-08-29 19:36:49 +0200

    axios & bootstrap installed

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js")
+import "./assets/css/bootstrap.min.css";
+import "./assets/js/bootstrap.min.js";
 
 createApp(App).use(store).use(router).mount('#app')
