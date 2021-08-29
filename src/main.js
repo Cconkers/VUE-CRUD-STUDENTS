a03c06070ea9cfb10a02248eaa57cca6a0a560fb
Mohamed Aberkani <aberkanimed@gmail.com>
2021-08-29 22:22:55 +0200

    Plantilla Admin Integrada

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js")
-import "./assets/css/bootstrap.min.css";
-import "./assets/js/bootstrap.min.js";
+import "./assets/css/portal.css";
+import "./assets/js/app.js";
 
 createApp(App).use(store).use(router).mount('#app')
